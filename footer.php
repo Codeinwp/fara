5751cd96e82cce57e82dd889604b93a38582c076
abaicus <baicusandrei@me.com>
2015-12-16 11:30:50 +0200

    !!!Added rel=nofollow to wordpress footer link.

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'fara' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'fara' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'fara' ) ); ?>" rel="nofollow"><?php printf( __( 'Proudly powered by %s', 'fara' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %2$s by %1$s.', 'fara' ), 'JustFreeThemes', '<a href="http://justfreethemes.com/fara" rel="nofollow">Fara</a>' ); ?>
 			<a href="#" class="scrolltop"><i class="fa fa-chevron-up"></i>&nbsp;<?php _e('Back to top ', 'fara'); ?></a>
