1308b483d1de147b1640bb0911564af095e92b5c
dragosey <dragos.n.sandu@gmail.com>
2017-08-16 15:32:04 +0300

    #11 Nivo Slider shortcode is not working

--- header.php
+++ header.php
@@ -57,7 +57,8 @@
 	<?php if ( ( is_front_page() && get_theme_mod('fara_slider_front') ) || ( (is_home() || is_archive()) && get_theme_mod('fara_slider_home') ) || ( is_singular() && get_theme_mod('fara_slider_singular') ) ) : ?>
 		<div class="featured-area">
 		<?php $shortcode = get_theme_mod('fara_meta_shortcode');
-	    	echo do_shortcode(esc_attr($shortcode));
+		    $scd = html_entity_decode( $shortcode );
+		    echo do_shortcode( $scd );
 	    ?>
 		</div>
 	<?php endif; ?>
